447ca0602e0f2d80c5aa2df0fd27fa98906fe311
paulalbo <paul-albrecht@gmx.at>
2024-02-03 11:53:39 +0100

    refactor hero img to slider

--- script/script.js
+++ script/script.js
@@ -3,3 +3,29 @@ import {features} from './features.js';
 await features();
 
 /* insert custom scripts below */
+
+
+    var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 30,
+        grabCursor: true,
+        effect: "creative",
+        creativeEffect: {
+          prev: {
+            shadow: true,
+            translate: ["-20%", 0, -1],
+          },
+          next: {
+            translate: ["100%", 0, 0],
+          },
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+    });
+
+    
